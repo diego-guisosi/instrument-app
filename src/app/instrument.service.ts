b864843362f8f0b96ed28861c8732fca486b602e
Diego <diego.guisosi@gmail.com>
2020-02-08 12:10:38 -0300

    add REST API call to instruments

--- src/app/instrument.service.ts
+++ src/app/instrument.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Instrument } from './instrument';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstrumentService {
 
+  url = 'http://localhost:8080/instruments';
+
   constructor(
     private http: HttpClient
   ) { }
 
   getAll() {
-    return this.http.get('/assets/instruments.json');
-    //return this.http.get('http://localhost:8080/instruments');
+    return this.http.get<Instrument[]>(this.url);
   }
 
 }
---
+++ src/app/instrument.ts
@@ -0,0 +1,7 @@
+export interface Instrument {
+
+  id: number;
+  symbol: string;
+  maturityDate: Date;
+
+}
